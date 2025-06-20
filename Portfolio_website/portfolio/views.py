758d72167fb59fb396304a9425c611cbfe81e1d1
KaviyaRaajenthiran <kaviyaraajenthiran@gmail.com>
2025-06-20 15:37:57 +0530

    feat:projects page updated

--- Portfolio_website/portfolio/views.py
+++ Portfolio_website/portfolio/views.py
@@ -6,3 +6,14 @@ def home(request):
 
 def about(request):
   return render(request,'portfolio/about.html')
+
+def projects(request):
+  projects_show = [
+    {'title':'Portfolio',
+     'path':'portfolio/images/portfolio image.png'
+     },
+     {'title':'Food App',
+     'path':'portfolio/images/portfolio image.png'
+     }
+  ] 
+  return render(request,'portfolio/projects.html',{'projects_show':projects_show})
